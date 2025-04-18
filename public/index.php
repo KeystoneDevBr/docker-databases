37ba13f64cdc6bbdc8c98dee4b4d805457895c13
Fagne Reges <fagnereges@MacBook-Pro-de-Fagne.local>
2025-04-18 01:01:10 -0300

    MariaDB admin with port 44443 ok

--- public/index.php
+++ public/index.php
@@ -83,7 +83,7 @@ echo '<!DOCTYPE html>
     <div class="container">
         <div class="box" onclick="window.location.href=\'./phpinfo.php\'">PHP Info</div>
         <div class="box" onclick="window.location.href=\'https://127.0.0.1:4443\'">MYSQL Admin</div>
-        <div class="box" onclick="window.location.href=\'https://www.php.net\'">PHP</div>
+        <div class="box" onclick="window.location.href=\'https://127.0.0.1:44443\'">MariaDB Admin</div>
     </div>
 </body>
 </html>';
